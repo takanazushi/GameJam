4bdd30aebc13d04de3f66bdd1edb594b8ae4bc50
yukari <c1217@oic.jp>
2024-01-30 01:16:27 +0900

    ゲーム基本的な処理追加

--- YusyaMao/Assets/Script/HeroSide/GameManager.cs
+++ YusyaMao/Assets/Script/HeroSide/GameManager.cs
@@ -4,5 +4,68 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    /// <summary>
+    /// インスタンス
+    /// </summary>
+    public static GameManager Instance;
+
+    /// <summary>
+    /// ゲーム操作可能フラグtrue:可能
+    /// </summary>
+    bool GameOperationFlg;
+    public bool GetGameOperationFlg
+    {
+        get { return GameOperationFlg; }
+        set { GameOperationFlg = value; }
+    }
+
+    /// <summary>
+    /// 制限時間、秒
+    /// </summary>
+    [SerializeField]
+    float Time_limit;
+
+    /// <summary>
+    /// 制限時間を計測するかtrue:計測する
+    /// </summary>
+    [SerializeField]
+    bool IsTime_Count;
+
+    public float GetTime_limit
+    {
+        get { return Time_limit;}
+    }    
+
+    private void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (IsTime_Count)
+        {
+            //経過時間を増加
+            Time_limit -= Time.deltaTime;
+
+            if (Time_limit <= 0.0f)
+            {
+                Time_limit = 0.0f;
+                IsTime_Count = false;
+
+            }
+
+        }
+
+    }
+
 
 }
---
+++ YusyaMao/Assets/Script/HeroSide/UI/UI_Time.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Time : MonoBehaviour
+{
+    Text UI_text;
+
+    private void Awake()
+    {
+        UI_text= GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        UI_text.text=
+        GameManager.Instance.GetTime_limit.ToString("N1"); 
+    }
+}
