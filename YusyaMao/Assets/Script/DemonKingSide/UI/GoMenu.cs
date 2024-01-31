c2ffa08b13fd1f924618005d84eb0fc6e2a50a6d
田中紗世 <C2178@oic.jp>
2024-01-31 10:13:21 +0900

    メニュー追加

---
+++ YusyaMao/Assets/Script/DemonKingSide/UI/GoMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoMenu : MonoBehaviour
+{
+    public GameObject PauseBack;
+    private bool Active;
+
+    private void Update()
+    {
+        PauseBack.SetActive(Active);
+    }
+
+    public void OnButton()
+    {
+        Active = !Active;
+    }
+}
